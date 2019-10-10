ebd4b05c4c6e99ee996dfe316926dd3178f025f5
tom.debek@gmail.com <tom.debek@gmail.com>
2019-10-10 21:25:56 +0200

    Add structure comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,10 +4,9 @@ class CommentsController < ApplicationController
   before_action :set_comment, only: [:edit, :destroy, :update,]
 
   def create
-    #@post = Post.find(params[:post_id])
     @comment = @post.comments.create(params[:comment_params])
     @comment.user_id = current_user.id if current_user
-    @comment.save
+    #@comment.save
 
     if @comment.save
       redirect_to post_path(@post)
@@ -17,13 +16,9 @@ class CommentsController < ApplicationController
   end
 
   def edit
-    # @post = Post.find(params[:post_id])
-    #@comment = Comment.find(params[:id])
   end
 
   def destroy
-    # @post = Post.find(params[:post_id])
-    #  @comment = Comment.find(params[:id])
     if @comment.destroy
       redirect_to post_path(@post)
     else
@@ -32,8 +27,6 @@ class CommentsController < ApplicationController
   end
 
   def update
-    #@post = Post.find(params[:post_id])
-    #@comment = Comment.find(params[:id])
     if @comment.update(comment_params)
       redirect_to post_path(@post)
     else
